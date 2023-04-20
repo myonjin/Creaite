7d6f3c082a9885fd27e45cea60477c197fb1a6a8
zzangbae <bestisland01@gmail.com>
2023-04-20 09:33:09 +0900

    first commit

---
+++ src/main/java/D6B/D_discover_user/DDiscoverUserApplication.java
@@ -0,0 +1,13 @@
+package D6B.D_discover_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDiscoverUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDiscoverUserApplication.class, args);
+	}
+
+}
