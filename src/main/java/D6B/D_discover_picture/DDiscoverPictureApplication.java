3e7ae1bc2e36ac0f479a59439a1476907be658eb
zzangbae <bestisland01@gmail.com>
2023-04-20 09:41:53 +0900

    first commit

---
+++ src/main/java/D6B/D_discover_picture/DDiscoverPictureApplication.java
@@ -0,0 +1,13 @@
+package D6B.D_discover_picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDiscoverPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDiscoverPictureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/D6B/D_discover_picture/DDiscoverPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package D6B.D_discover_picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDiscoverPictureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
