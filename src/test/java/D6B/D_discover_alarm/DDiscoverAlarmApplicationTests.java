e67d2f3bfc4513597b58843ad7deec49578acb5b
zzangbae <bestisland01@gmail.com>
2023-04-20 09:34:28 +0900

    first commit

---
+++ src/test/java/D6B/D_discover_alarm/DDiscoverAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package D6B.D_discover_alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDiscoverAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
