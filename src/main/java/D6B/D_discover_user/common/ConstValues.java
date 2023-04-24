7c371957df45fc097377780eb3e802ed3e1711e3
zzangbae <bestisland01@gmail.com>
2023-04-24 10:59:14 +0900

    feat : 엔티티 매핑, 아직 수정가능성 있음

---
+++ src/main/java/D6B/D_discover_user/common/ConstValues.java
@@ -0,0 +1,7 @@
+package D6B.D_discover_user.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConstValues {
+}
---
+++ src/main/java/D6B/D_discover_user/user/domain/Like.java
@@ -0,0 +1,28 @@
+package D6B.D_discover_user.user.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    // 좋아요를 누른 유저 객체
+    @ManyToOne
+    @JoinColumn(name = "user_id")   // 매핑은 유저 id랑 해놓기
+    private User user;
+
+    // 좋아요가 눌러진 그림 -> MSA라 서비스가 따로 있어서 ManyToOne은 되지 않는다.
+    @Column(name = "picture_id", nullable = false)
+    private Long pictureId;
+}
