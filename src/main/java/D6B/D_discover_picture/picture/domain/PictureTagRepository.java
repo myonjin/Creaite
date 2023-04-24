f9a477fc76bc4bfad1df2e35e76efcf034fed539
zzangbae <bestisland01@gmail.com>
2023-04-24 15:57:32 +0900

    feat : 이미지가 생성된 것인지, 편집인지 판단하는 컬럼 추가함

--- src/main/java/D6B/D_discover_picture/picture/domain/PictureTagRepository.java
+++ src/main/java/D6B/D_discover_picture/picture/domain/PictureTagRepository.java
@@ -2,5 +2,5 @@ package D6B.D_discover_picture.picture.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PictureTagRepository extends JpaRepository {
+public interface PictureTagRepository extends JpaRepository<PictureTag, Long> {
 }
