910c837d3384ad76aa5f6010571a3b7ae6b4baef
zzangbae <bestisland01@gmail.com>
2023-04-24 15:50:45 +0900

    feat : JPA로 db매핑

--- src/main/java/D6B/D_discover_alarm/domain/Alarm.java
+++ src/main/java/D6B/D_discover_alarm/domain/Alarm.java
@@ -2,10 +2,7 @@ package D6B.D_discover_alarm.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Builder
@@ -15,18 +12,35 @@ import java.time.Instant;
 @Setter
 @ToString
 @Entity
+@Table(name = "alarm")
 public class Alarm {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "sender_id")
     private Long senderId;  // 좋아요를 누른사람
+
+    @Column(name = "receiver_id")
     private Long receiverId;    // 좋아요 받은 이미지를 만든사람
+
+    @Column(name = "picture_id")
     private Long pictureId;     // 좋아요 받은 이미지 id
+
+    @Column(name = "content")
     private String content;     // 알람 내용
+
+    @Column(name = "created_at")
     private Instant createdAt;  // 알람 생성일
+
+    @Column(name = "is_sended")
     private Boolean isSended;
+
+    @Column(name = "is_read")
     private Boolean isRead;
+
+    @Column(name = "type")
     private Integer type;
 
 }
