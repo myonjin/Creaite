95d4ca43f6ad8e2adf9420933e53cfd7575480ca
binsid1 <binsid1@naver.com>
2023-05-17 17:27:43 +0900

    feat: 다운로드 페이지 단순하게 제작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/applogo.png";
+import middleImage from "./assets/middle.png";
+import oneDown from "./assets/one_downloadbadge_red_black.png";
+import apkDown from "./assets/apkDownload.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="headerStyle">
+        <div>
+          {/* <img src={logo} className="imageStyle" /> */}
+          <p className="textStyle">CreAite</p>
+        </div>
       </header>
+      <hr />
+      <div>
+        <img src={middleImage} className="middleStyle" />
+      </div>
+      <br />
+      <br />
+      <div className="boxStyle">
+        <a href="">
+          <img src={oneDown} className="imageStyle" />
+        </a>
+        <div className="box"></div>
+        <a href="">
+          <img src={apkDown} className="imageStyle" />
+        </a>
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
